92cf423969b5ed01ef7608d4d806b7b9892bf378
YOUSSEF HAKIM <joe.joseph957@yahoo.com>
2024-09-18 23:22:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const imageContainerEl = document.querySelector(".image-container")
+
+const btnEl = document.querySelector(".btn")
+
+let imageNum = 10;
+btnEl.addEventListener("click", () => {
+    addNewImages()
+});
+
+function addNewImages() {
+    for (let index = 0; index < imageNum; index++) {
+        const newImgEl = document.createElement("img")
+        newImgEl.src=`
+        https://picsum.photos/300?random=${Math.floor
+            (Math.random() * 2000
+        )}`;
+        imageContainerEl.appendChild(newImgEl);
+        
+    }
+};
